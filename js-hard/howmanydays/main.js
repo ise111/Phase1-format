f336c5131aee285b191ce05cddd2d46959d39e00
ise111 <isosa5454@gmail.com>
2021-11-08 20:59:23 +0900

    211108-2

--- js-hard/howmanydays/main.js
+++ js-hard/howmanydays/main.js
@@ -2,46 +2,21 @@ const relativeTimes = document.getElementById('relativeTimes');
 const dateSet = document.getElementById('dateSet');
 const dateSearch = document.getElementById('dateSearch');
 const diffTime = document.getElementById('diffTime');
-
 // ドラえもん誕生日
-const doraBorn = moment('2112-09-03 00:00:00');
+const doraBorn = moment('2112-09-03');
 
-// 誕生日までの残り時間表示関数
-function nowRelativeTimesToDora() {
-
+// targetまでの残り時間表示関数
+function sample(targetDay, text) {
     let now = moment();
-    let relativeYears = doraBorn.diff(now, 'years');
-    let relativeMonths = doraBorn.diff(now, 'months') % 12;
-    let relativeDays = doraBorn.diff(now, 'days') % 365;
-    let relativeHours = doraBorn.diff(now, 'hours') % 24;
-    let relativeMinutes = doraBorn.diff(now, 'minutes') % 60;
-    let relativeSeconds = doraBorn.diff(now, 'seconds') % 60;
-
-    relativeTimes.textContent = `${relativeYears}年${relativeMonths}ヶ月${relativeDays}日${relativeHours}時間${relativeMinutes}分${relativeSeconds}秒`;
-
-}
-
-// 任意の日までの残り時間表示関数
-function nowRelativeTimesToSet() {
-
-    let getDate = moment(`${dateSet.value} 00:00:00`);
-    let now = moment();
-    let relativeYears = getDate.diff(now, 'years');
-    let relativeMonths = getDate.diff(now, 'months') % 12;
-    let relativeDays = getDate.diff(now, 'days') % 365;
-    let relativeHours = getDate.diff(now, 'hours') % 24;
-    let relativeMinutes = getDate.diff(now, 'minutes') % 60;
-    let relativeSeconds = getDate.diff(now, 'seconds') % 60;
-    
-    diffTime.textContent = `${dateSet.value} 00:00:00まで${relativeYears}年${relativeMonths}ヶ月${relativeDays}日${relativeHours}時間${relativeMinutes}分${relativeSeconds}秒`;
-
+    let duration = moment.duration(targetDay.diff(now))['_data'];
+    text.textContent = `${targetDay.format('YYYY-MM-DD')} 00:00:00まで${duration['years']}年${duration['months']}ヶ月${duration['days']}日${duration['hours']}時間${duration['minutes']}分${duration['seconds']}秒`;
 }
 
 // 関数呼び出しと１秒毎に更新
-setInterval(nowRelativeTimesToDora, 1000);
+setInterval('sample(doraBorn, relativeTimes)', 1000);
 // 検索ボタンのイベント
 dateSearch.addEventListener('click', function() {
-
-    setInterval(nowRelativeTimesToSet, 1000);
     
+    setInterval('sample(moment(dateSet.value), diffTime)', 1000);
+
 });
---
+++ js-hard/numeron/main.js
@@ -0,0 +1,92 @@
+const setNum = document.getElementById('answerNum');
+const numCheckBtn = document.getElementById('numCheck');
+const remainTurn = document.getElementById('remainTurn');
+
+// 初期値
+let eatCount = 0;
+let biteCount = 0;
+let turn = 10;
+let nums = [
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+];
+let cpNums = [];
+
+// cpの数字決定関数
+function randomNum() {
+    cpNums = [];
+    let cpNumIndex1 = Math.floor(Math.random() * 10);
+    let cpNumIndex2 = Math.floor(Math.random() * 10);
+    let cpNumIndex3 = Math.floor(Math.random() * 10);
+    
+    // 数字の重複防止
+    while (cpNumIndex1 === cpNumIndex2) {
+        cpNumIndex2 = Math.floor(Math.random() * 10);
+    }
+    
+    while (cpNumIndex1 === cpNumIndex3 || cpNumIndex2 === cpNumIndex3) {
+        cpNumIndex3 = Math.floor(Math.random() * 10);
+    }
+
+    cpNums.push(nums[0][cpNumIndex1]);
+    cpNums.push(nums[1][cpNumIndex2]);
+    cpNums.push(nums[2][cpNumIndex3]);
+    console.log(cpNums);
+}
+
+// 判定関数
+function judge(nums) {
+
+    for (let i = 0; i < 3; i++) {
+        if (nums[i] === cpNums[i]) {
+            eatCount++;
+        } else if (nums[i] === cpNums[(i + 1) % 3] || nums[i] === cpNums[(i + 2) % 3]) {
+            biteCount++;
+        }
+    }
+
+    alert(`${eatCount}EAT,${biteCount}BITE`);
+
+    if (eatCount === 3) {
+        alert('クリア！！');
+        turn = 10;
+        randomNum();
+    }
+}
+
+// 答え合わせボタンイベント
+numCheckBtn.addEventListener('click', function() {
+    eatCount = 0;
+    biteCount = 0;
+    turn --;
+    let answerNums = Array.from(setNum.value);
+    
+    if (answerNums[0] === answerNums[1] || answerNums[1] === answerNums[2] || answerNums[2] === answerNums[0]) {
+        alert('同じ数字を入れないでください');
+        setNum.value = '';
+        return;
+    }
+    
+    if (answerNums.length !== 3) {
+        alert('3桁の数字を入力してください');
+        setNum.value = '';
+        return;
+    }
+    
+    judge(answerNums);
+    
+    if (turn === 0) {
+        alert('ターンを使い切りました。あなたの負けです。');
+        turn = 10;
+        randomNum();
+    }
+    remainTurn.textContent = `残り${turn}回です。`;
+    setNum.value = '';
+});
+
+
+remainTurn.textContent = `残り${turn}回です。`;
+// 関数呼び出し
+randomNum();
+
